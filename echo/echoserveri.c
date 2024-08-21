315ae80608dc83a3f60c1bb293df91ac4e69a6f8
ll5215 <foxlover17@gmail.com>
2024-08-21 13:45:01 +0000

    test echo

--- echo/echoserveri.c
+++ echo/echoserveri.c
@@ -27,3 +27,15 @@ int main(int argc, char **argv)
     }
     exit(0);
 }
+
+void echo(int connfd) {
+    size_t n;
+    char buf[MAXLINE];
+    rio_t rio;
+
+    Rio_readinitb(&rio, connfd);
+    while((n = Rio_readlineb(&rio, buf, MAXLINE)) != 0) {
+        printf("server received %zu bytes\n", n);
+        Rio_writen(connfd, buf, n);
+    }
+}
